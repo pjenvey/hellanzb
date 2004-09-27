b341aa8ff1445dbf27f110fa60aecdedf9d081f8
Ben Bangert <ben@groovie.org>
2004-09-27 06:06:08 +0000

    Added documentation, todo list.

--- hellagrowler.py
+++ hellagrowler.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python
+"""
+hellagrowler.py
+Date: 9/26/04
+Author: Ben Bangert
+
+hellagrowler is a XML-RPC daemon for OSX that delivers messages to the user through
+Growl user notification daemon.
+
+For information on Growl: http://growl.info/
+
+INSTRUCTIONS
+Before using this, you must have installed several packages in OSX.
+
+1) Install Growl (http://growl.info/downloads.php)
+2) Install PyObjC (http://pyobjc.sourceforge.net/)
+3) Install the Growl Python Bindings (http://growl.info/downloads.php)
+4) Run hellagrowler.py
+
+TODO
+o Put all that install crap into a nice OSX Package Installer, and run this as a
+  system service.
+o Put some basic authentication (require password/secret) so it won't just spit
+  out anything from anyone
+"""
 
 import Growl
 import SimpleXMLRPCServer

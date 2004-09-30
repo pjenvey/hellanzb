3613ebcca58a29edb0f3c30f8f316c49b7518b24
Philip Jenvey <pjenvey@underboss.org>
2004-09-30 04:49:17 +0000

    build stuff -- o added -l and -t options to the build script o added a darwinport (not done yet), and supporting it in the build script o moved the port dir to freebsd-port

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ distribution, and finally check in the version number bump change
 @author pjenvey
 
 """
-import os, re, setup, sys
+import optparse, os, re, setup, sys, tarfile
 from Hellanzb.Util import assertIsExe, stringEndsWith
 
 __id__ = '$Id$'
@@ -43,24 +43,71 @@ def buildDist():
 def buildPort(version):
     """ build a FreeBSD port """
     print "Building new FreeBSD port"
+    portStubDir = 'freebsd-port'
     
-    portDir = 'dist/hellanzb-port'
-    if not os.path.isdir(portDir):
-        os.mkdir(portDir)
+    portDestDir = 'dist/hellanzb-freebsd-port-' + version
+    destDir = portDestDir + os.sep + 'hellanzb'
+
+    if not os.path.isdir(portDestDir):
+        os.mkdir(portDestDir)
+    if not os.path.isdir(destDir):
+        os.mkdir(destDir)
 
     # replace the version
-    os.system('cat port/Makefile | sed s/____VERSION____/' + version + '/ > ' + portDir + '/Makefile')
+    os.system('cat ' + portStubDir + os.sep + 'Makefile | sed s/____VERSION____/' + version + '/ > ' +
+              destDir + os.sep + 'Makefile')
 
     # copy over the pkg-descr file
-    input = file('port/pkg-descr')
+    input = file(portStubDir + os.sep + 'pkg-descr')
     lines = input.readlines()
     input.close()
     
-    output = file(portDir + '/pkg-descr', 'w')
+    output = file(destDir + os.sep + 'pkg-descr', 'w')
     output.writelines(lines)
     output.close()
+
+    dir = portDestDir[len('dist/'):]
+    createTarBall('dist', dir, dir + '.tar.gz')
+
+def buildDPort(version):
+    print "Building new Darwin port"
+    portStubDir = 'darwin-dport'
+    
+    portDestDir = 'dist/hellanzb-darwin-dport-' + version
+    destDir = portDestDir + os.sep + 'hellanzb'
+
+    if not os.path.isdir(portDestDir):
+        os.mkdir(portDestDir)
+    if not os.path.isdir(destDir):
+        os.mkdir(destDir)
+
+    # replace the version
+    os.system('cat ' + portStubDir + os.sep + 'Portfile | sed s/____VERSION____/' + version + '/ > ' +
+              destDir + os.sep + 'Portfile')
+
+    dir = portDestDir[len('dist/'):]
+    createTarBall('dist', dir, dir + '.tar.gz')
+
+def createTarBall(workingDir, dirName, fileName):
+    """ tar -cxvf """
+    cwd = os.getcwd()
+    os.chdir(workingDir)
+    
+    tarBall = tarfile.open(fileName, 'w:gz')
+    for file in os.listdir(dirName):
+        tarBall.add(dirName + os.sep + file)
+    tarBall.close()
+        
+    os.chdir(cwd)
     
 try:
+    parser = optparse.OptionParser()
+    parser.add_option('-l', '--local', action='store_true', dest='local',
+                      help='Do a local build (don\'t commit version changes')
+    parser.add_option('-t', '--trunk', action='store_true', dest='head',
+                      help='Assume this is a trunk (HEAD) build, and do not bump the version number')
+    options, args = parser.parse_args()
+
     assertIsExe('svn')
 
     versionFile = open(VERSION_FILENAME)
@@ -74,22 +121,28 @@ try:
     version = re.sub(r'\'', r'', versionLine)
 
     if stringEndsWith(version, '-HEAD'):
-        # Bump the version to a stable number
-        version = version[0:-len('-HEAD'):]
-        newVersion = bumpVersion(version)
-        writeVersion(newVersion)
+        if not options.head:
+            # Bump the version to a stable number
+            version = version[0:-len('-HEAD'):]
+            newVersion = bumpVersion(version)
+            writeVersion(newVersion)
+            version = newVersion
 
         # Build
-        print 'Building version: ' + newVersion
-        setup.version = newVersion
+        print 'Building version: ' + version
+        setup.version = version
         buildDist()
-        buildPort(newVersion)
+        buildPort(version)
+        buildDPort(version)
 
-        # Append -HEAD back to the number and check in bump
-        newVersion = newVersion + '-HEAD'
-        print 'Checking in new version number: ' + newVersion
-        writeVersion(newVersion)
-        os.system('svn ci -m "New build, version: ' + newVersion + '" ' + VERSION_FILENAME)
+        if not options.head:
+            # Append -HEAD back to the number and check in bump
+            newVersion = version + '-HEAD'
+            writeVersion(version)
+        
+        if not options.local:
+            print 'Checking in new version number: ' + version
+            os.system('svn ci -m "New build, version: ' + version + '" ' + VERSION_FILENAME)
 
     else:
         print 'Error: Version number: ' + version + ' is not HEAD!'

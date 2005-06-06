805e46b2dbcfdc9882df1b33c3400793385f4f1d
Philip Jenvey <pjenvey@underboss.org>
2005-06-06 07:49:13 +0000

    adding new HellaXMLRPC module to setup packages. thanks No1UKnow

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def runSetup():
         url = 'http://www.hellanzb.com',
         license = 'BSD',
 
-        packages = [ 'Hellanzb', 'Hellanzb.NZBLeecher' ],
+        packages = [ 'Hellanzb', 'Hellanzb.NZBLeecher', 'Hellanzb.HellaXMLRPC' ],
         scripts = [ 'hellanzb.py', 'hellagrowler.py' ],
         data_files = [ ( 'etc', [ 'etc/hellanzb.conf.sample' ] ),
                        ( 'share/doc/hellanzb', [ 'CREDITS', 'README', 'LICENSE' ] ) ],

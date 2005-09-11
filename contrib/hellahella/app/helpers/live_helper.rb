5e25f46650d3079dc9be189ffd06a3d19151426c
Ben Bangert <ben@groovie.org>
2005-09-11 23:55:46 +0000

    Added ETA

--- contrib/hellahella/app/helpers/live_helper.rb
+++ contrib/hellahella/app/helpers/live_helper.rb
@@ -1,2 +1,8 @@
 module LiveHelper
+  def eta(seconds)
+    hours = seconds / 60 / 60
+    minutes = seconds / 60 % 60
+    secs = seconds % 60
+    "%02d:%02d:%02d" % [hours, minutes, secs]
+  end
 end

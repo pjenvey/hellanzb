e0115a919a03ebb09b31e70d93b80e2eb4de566e
Philip Jenvey <pjenvey@underboss.org>
2005-09-21 06:13:14 +0000

    added a Pause/Continue button to hellahella

--- contrib/hellahella/app/controllers/live_controller.rb
+++ contrib/hellahella/app/controllers/live_controller.rb
@@ -13,6 +13,16 @@ class LiveController < ApplicationController
     else
       server.call('pause')
     end
+    
+    # FIXME: calling this function screws up the output returned to javascript
+    # (AJAX). Doing it manually
+    #get_status_queue(:force => true)
+    
+    session[:cache].merge!({:status => server.call("status"),
+                             :queuelist => server.call('list'),
+                             :time => Time.at(Time.now+4)})
+    
+    render_text "new Ajax.Updater('hellastatus', '/live/status', {asynchronous:true, evalScripts:true})"
   end
 
   def enqueue_nzb

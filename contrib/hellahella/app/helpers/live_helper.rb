3844ec1fa2d26e2af59195d7b2bf8cd6eb94d440
Ben Bangert <ben@groovie.org>
2006-03-28 00:33:48 +0000

    Wiping obsolete Rails version of hellahella

--- contrib/hellahella/app/helpers/live_helper.rb
+++
@@ -1,8 +0,0 @@
-module LiveHelper
-  def eta(seconds)
-    hours = seconds / 60 / 60
-    minutes = seconds / 60 % 60
-    secs = seconds % 60
-    "%02d:%02d:%02d" % [hours, minutes, secs]
-  end
-end
--- contrib/hellahella/config/routes.rb
+++
@@ -1,8 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  
-  map.connect 'queue/:url', :controller => 'hellanzb', :action => 'enqueue_bookmarklet'
-  
-  map.connect ':controller/:action/:id'
-  
-  map.connect '', :controller => 'hellanzb', :action => 'index'
-end

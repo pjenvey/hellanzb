77048f71940fdc4e5e6696611ed95baa0b7417c5
Philip Jenvey <pjenvey@underboss.org>
2006-04-30 22:21:06 +0000

    nudgeIdleNZBLeechers was calling fetchNextNZBSegment directly, causing concurrency trouble (e.g.: 'exceptions.ValueError: I/O operation on closed file'. Now calling it via callLater

--- Hellanzb/NZBLeecher/NZBSegmentQueue.py
+++ Hellanzb/NZBLeecher/NZBSegmentQueue.py
@@ -431,7 +431,7 @@ class NZBSegmentQueue(PriorityQueue):
         if not Hellanzb.downloadPaused and not requeuedSegment.nzbFile.nzb.canceled:
             for nsf in Hellanzb.nsfs:
                 if nsf.serverPoolName not in requeuedSegment.failedServerPools:
-                    nsf.fetchNextNZBSegment()
+                    reactor.callLater(0, nsf.fetchNextNZBSegment)
 
     def fileDone(self, nzbFile):
         """ Notify the queue a file is done. This is called after assembling a file into it's

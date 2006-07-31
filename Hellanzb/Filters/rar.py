765d9fab0234a04a8215f6d8139ebf7c0b881032
Alexander Botero-Lowry <alex@foxybanana.com>
2006-07-31 01:39:00 +0000

    Add the actual checking of the rar header to determine encryption, accidently missed in the last commit.

--- Hellanzb/Filters/rar.py
+++ Hellanzb/Filters/rar.py
@@ -2,63 +2,73 @@ from Hellanzb.Filters import Filter, getFileExtension
 from Hellanzb.PostProcessorUtil import Topen
 
 def isRar(filename):
-	""" This implements a magic check for Rar files """
-	rarHeader = 'Rar!'
-	if not os.path.isfile(filename):
-		return False
-
-	ext = getFileExtension(filename)
-	if ext and ext == 'rar':
-		return True
-
-	f = file(filename)
-	if f.read(4) == rarHeader:
-		f.close()
-		return True
-
-class RarFilter(Filter):
-	def __init__(self):
-		pass
-
-	def groupAlikes(self, files):
-		# Make a list of main_rars, we only deal with the first one
-		# in the list, and then we return this so it can be removed
-		# from the file list and then the new file list can be iterated
-		# by the filter handler.
-		mainRars = []
-		for a in files:
-			if a.endswith('.rar'):
-				mainRars.append(a)
-
-		# Now we determine the rar group name, and then we get a list of
-		# files that match the two common patterns with that rar group name.
-		theRar=mainRars[0]
-		_splitRar = theRar.split('.')
-		groupName = '.'.join(_splitRar[:len(_splitRar)-1])
-		groupMembers = [ a for a in files if re.search(groupName+'\.r([0-9]{1,4})', b) or re.search(groupName+'\.part([0-9]{1,4})', a) ]
-
-		return groupMembers
-
-	def canHandle(self, fileName):
-        if isRar(fileName):
-            return True
-        else:
+    """ This implements a magic check for Rar files """
+    rarHeader = 'Rar!'
+    if not os.path.isfile(filename):
             return False
 
-	def processFile(self, files):
-		""" Process a file """
-		# In a non-encrypted header offset 23=t and offset 24 either ' '
-		# if not encypted or '$' if encrypted. If the t isn't there. The
-		# header is most likely encypted. An unencrypted header will also 
-		# have an R at offset 32. 
-		# With encrypted headers the check is obvious, with unencrypted
-		# headers, things are a bit more difficult because we can only easily
-		# tell if the first file is encrypted not if all of them are. 
+    ext = getFileExtension(filename)
+    if ext and ext == 'rar':
+            return True
 
-		# FIXME: this doesn't actually account for cases where there are 
-		# comments in the rar, and thus isn't very useful. 
-		f = file(filename)
-		buf = f.read(64)
-		if buf[22] != 't' and buf[31] != 'R':
-			encryptedRar = True
+    f = file(filename)
+    if f.read(4) == rarHeader:
+            f.close()
+            return True
 
+class RarFilter(Filter):
+    def __init__(self):
+        pass
+
+    def groupAlikes(self, files):
+        """ Returns the a Rar group the filter is able to act on """
+        # Make a list of main_rars, we only deal with the first one
+        # in the list, and then we return this so it can be removed
+        # from the file list and then the new file list can be iterated
+        # by the filter handler.
+        mainRars = []
+        for a in files:
+            if a.endswith('.rar'):
+               mainRars.append(a)
+
+        # Now we determine the rar group name, and then we get a list of
+        # files that match the two common patterns with that rar group name.
+        theRar=mainRars[0]
+        _splitRar = theRar.split('.')
+        groupName = '.'.join(_splitRar[:len(_splitRar)-1])
+        groupMembers = [ a for a in files if re.search(groupName+'\.r([0-9]{1,4})', b) or re.search(groupName+'\.part([0-9]{1,4})', a) ]
+
+        return groupMembers
+
+    def canHandle(self, files):
+        pass
+
+    def processFile(self, files):
+        """ Process a file """
+        # First file should be the .rar, so we'll check the encryption
+        # state on the main rar file.
+        _fileName = files[0]
+        f = file(_fileName)
+        buf = f.read(22)
+        # If the header of a rar is encrypted, the binary AND of the 
+        # 10th and 11th byte AND 0x80 will be non-zero. This is the
+        # encrypted header flag.
+        if ( ord(b[10])+ ord(b[11]) ) & 0x80 != 0:
+            rarEncrypted=True
+
+        # Determine if the next block is a Comment and if it is
+        # determine its size and seek past it. 
+        buf = f.read(9)
+        if hex(ord(b[0])) == '0x7a':
+            head_size = ord(b[3]) + ord(b[4])
+            data_size = ord(b[5]) + ord(b[6]) + ord(b[7]) + ord(b[8])
+            seek_forward = (data_size+head_size)-9
+            buf = f.seek(seek_forward, 1)
+            buf = f.read(10)
+        # Now check the first file header. If the HEAD_FLAG for the file 
+        # header has a non-zero binary AND with 0x04, it is encrypted. 
+        if ord(b[1]) & 0x04:
+            rarEncrypted=True
+
+        # TODO: handle other files in the Rarchive
+        # TODO: methodize the header seek code.

b7624609b07dc020c7074ed25c0dc9185988c199
Alexander Botero-Lowry <alex@foxybanana.com>
2006-08-17 04:31:04 +0000

    Add some basic exceptions to be thrown by the filters under certain circumstances

--- Hellanzb/Filters/__init__.py
+++ Hellanzb/Filters/__init__.py
@@ -1,6 +1,20 @@
 import Hellanzb
 import os, string
 
+class FilterError(Exception):
+    """
+        This is thrown when a filter can temporarily not handle a group, but
+        would like to try again later (e.g. needs assembly first)
+    """
+    pass
+
+class UnhandleableError(Exception:
+    """
+        This is raised when a filters process method can not handle a group
+        it thought it could handle before. The group should not be passed
+        back to it.
+    """
+    pass
 
 class Filter:
     """ Base class for all filters, cannot be used directly """
